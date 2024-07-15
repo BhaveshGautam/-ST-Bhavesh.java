7ee0213b6f0b5346a0be48a91e2013feb4f64e45
Bhavesh Gautam <bhaveshgautam2003@gmail.com>
2024-07-15 14:18:51 +0530

    third commit

--- Week0/day3/array.java
+++ Week0/day3/array.java
@@ -1,6 +1,6 @@
 package day3;
-
-public class array {
+/*
+ * public class array {
     public static void displayArray(int[] arr)
     {
         for(int i=0;i<arr.length;i++)
@@ -9,7 +9,7 @@ public class array {
     }
     public static void main(String[] args)
     {
-      int[] arr =new int [10];
+      int[] arr =new int [10]; // the rest of index will have 
       arr[1]=5;
       arr[5]=7;
       arr[6]=9;
@@ -19,3 +19,39 @@ public class array {
     }
     
 }
+
+ */
+
+/*
+ * public class array{
+    public static int reverseinteger(int n){
+        int temp;
+        for(temp=0;n!=0;n/=10)
+      // initilization  int rev=0;
+      //condition  int num=n%10;
+        temp=temp*10+n%10;
+       // change num=num/10;
+       return temp;
+    }
+    public static void main(String[] args) {
+        int n=9234;
+        System.out.println(reverseinteger(n));
+    }
+}
+ */
+public class array{
+    public static int countdigits(int n){
+        int count=0;
+        while(n!=0){
+            n=n/10;// digits elemination
+            count++;
+        }
+        return count;
+     }
+     public static void main(String[] args) {
+        int n=1234;
+        System.out.println(countdigits(n));
+     }
+    
+}
+
